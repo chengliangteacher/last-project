b17056f6656867efaff101494536cb71a865b8b8
chengliang <935982601@qq.com>
2018-07-20 15:26:13 +0800

    add-model

---
+++ dataApp/dao/model/goods.js
@@ -0,0 +1,73 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var GoodsSchema = new Schema({
+    // 名称
+    goodsName: {
+        type: String
+    },
+    // 品类
+    goodsType: {
+        type: String
+    },
+    // 材质或制作方法
+    goodsMaterial: {
+        type: String
+    },
+    // 适用规格
+    goodsCanFor: {
+        type: String
+    },
+    // 专属规格
+    goodsOnlyFor: {
+        type: String
+    },
+    // 包装规格
+    goodsSize: {
+        type: String
+    },
+    // 口味
+    goodsTaste: {
+        type: String
+    },
+    // 特殊功用
+    goodsSpecial: {
+        type: String
+    },
+
+    // 产地
+    goodsRegion: {
+        type: String
+    },
+    // 出厂日期
+    goodsDate: {
+        type: String
+    },
+    // 保质期
+    goodsTime: {
+        type: String
+    },
+    // 供应商
+    goodsSupplier: {
+        type: String
+    },
+    // 特色说明
+    goodsIntro: {
+        type: String
+    },
+    // 价格
+    goodsPrice: {
+        type: Number
+    },
+    // 图片
+    goodsImg:[{
+		type: Schema.Types.ObjectId,
+		ref: "imgs"
+	}],
+    usersId: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+});
+
+mongoose.model("goods", GoodsSchema, "goods");
---
+++ dataApp/dao/model/services.js
@@ -0,0 +1,49 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ServiceSchema = new Schema({
+	// 名称
+	serviceName: {
+		type: String
+	},
+	// 营业执照号码
+	shopLicenceNum: {
+		type: String
+	},
+	// 服务类别
+	serviceType: {
+		type: String
+	},
+	// 排期
+	serviceSchedule: {
+		type: String
+	},
+	// 适用规格
+	serviceCanFor: {
+		type: String
+	},
+	// 服务规格
+	serviceDetial: {
+		type: String
+	},
+	// 耗时
+	serviceTime: {
+		type: String
+	},
+	// 服务员等级
+	serviceLevel: {
+		type: Number
+	},
+
+	// 价格
+	servicePrice: {
+		type: Number
+	},
+	//用户联系
+    usersId: {
+		type: Schema.Types.ObjectId,
+		ref: 'users'
+	},
+});
+
+mongoose.model("services", ServiceSchema, "services");
