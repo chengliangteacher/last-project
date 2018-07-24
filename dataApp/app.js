4667b4baa5eb6702779cd0577bd3577b72cc2943
houxuezhi <1079798690@qq.com>
2018-07-24 09:21:42 +0800

    houxuezhi7.24(2)

--- dataApp/app.js
+++ dataApp/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 require('./dao/database.js')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var orderWrapRouter =require("./routes/orderWrap")
 
 var app = express();
 
@@ -21,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/users', orderWrapRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
